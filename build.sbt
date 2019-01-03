4607066a444840cd25dd7f8960bc1c48066a5303
thanasis1101 <bollasath@yahoo.gr>
2019-01-03 02:27:46 +0200

    k-means added

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import sbt.util
+
 name := "outliers_detection"
 
 version := "0.1"
@@ -8,5 +10,6 @@ val sparkVersion = "2.4.0"
 
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
-  "org.apache.spark" %% "spark-sql" % sparkVersion
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion
 )
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,12 +1,66 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.col
 
+import scala.collection.mutable.ArrayBuffer
 
 object App {
 
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder.master("local").appName("OutliersDetection").getOrCreate()
-    val df = spark.read.format("csv").option("header", "false").load("src/main/resources/data.csv")
+
+    println("")
+    println("Define the input data")
+    println("=====================")
+    println("")
+    val data = spark.read.format("csv").option("inferSchema", "true").load("src/main/resources/data.csv")
+
+
+    // count average of both columns
+    val c0_avg = data.select(avg("_c0")).first().get(0).asInstanceOf[Double]
+    val c1_avg = data.select(avg("_c1")).first().get(0).asInstanceOf[Double]
+
+
+    println("")
+    println("Create feature vectors and generate a K-Means model")
+    println("===================================================")
+    println("")
+
+    spark.udf.register("toVector", (a: Object, b: Object) => {
+      if (a==null || b == null) {
+        Vectors.dense(c0_avg, c1_avg)
+      } else {
+        Vectors.dense(a.asInstanceOf[Double], b.asInstanceOf[Double])
+      }
+    })
+
+    val features = data.selectExpr("toVector(_c0, _c1) as feature")
+    val kmeans = new KMeans().setK(3).setFeaturesCol("feature").setPredictionCol("prediction")
+    val model = kmeans.fit(features)
+
+
+    println("")
+    println("Calculate cluster centers")
+    println("=========================")
+    println("")
+    val predicted = model.transform(features)
+
+    //predicted.show(300)
+
+    val min_distance = 3000000.0
+
+
+    spark.udf.register("calcDistance", (feature: org.apache.spark.ml.linalg.Vector, prediction: Int) => {
+      val current_cluster = model.clusterCenters.apply(prediction) // Find in which cluster the current point was predicted
+      Vectors.sqdist(feature, current_cluster)
+    })
+
+    val distances = predicted.selectExpr("calcDistance(feature, prediction) as distance")
+    println(distances.filter(col("distance") > min_distance).count())
+
 
   }
 
